92fafc8bcbd45e4a458fd7a1bed02e6e75550c62
Gabriel <gabrielteixeira.21@hotmail.com>
2023-10-14 21:41:07 -0300

    modificações nos enum, criação basica de service e controller de usuario, algumas correções nos tratamentos de erro

--- loja_gp2/src/main/java/br/com/loja_gp2/loja_gp2/model/Enum/EnumTipoPagamento.java
+++ loja_gp2/src/main/java/br/com/loja_gp2/loja_gp2/model/Enum/EnumTipoPagamento.java
@@ -1,5 +1,20 @@
 package br.com.loja_gp2.loja_gp2.model.Enum;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+import br.com.loja_gp2.loja_gp2.model.exceptions.ResourceBadRequestException;
+
 public enum EnumTipoPagamento {
-    CREDITO, AVISTA, BOLETO
+    CREDITO, AVISTA, BOLETO;
+
+    @JsonCreator
+    public static EnumTipoPagamento fromString(String value) throws ResourceBadRequestException {
+        for (EnumTipoPagamento pagamento : EnumTipoPagamento.values()) {
+            if(pagamento.name().equalsIgnoreCase(value)) {
+                return pagamento;
+            }
+        }
+
+        throw new ResourceBadRequestException( "valor do campo pagamento: "+value+" inválido");
+    }
 }
