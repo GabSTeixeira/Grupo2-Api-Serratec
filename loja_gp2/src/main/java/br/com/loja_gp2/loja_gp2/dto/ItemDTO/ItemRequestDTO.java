53e4232d2205ff431e18ccff63cd18ed127d4f1c
Gabriel <gabrielteixeira.21@hotmail.com>
2023-10-16 13:55:01 -0300

    Inicio da fabricação dos services de pedido e item, exclusão do item controller

--- loja_gp2/src/main/java/br/com/loja_gp2/loja_gp2/dto/ItemDTO/ItemRequestDTO.java
+++ loja_gp2/src/main/java/br/com/loja_gp2/loja_gp2/dto/ItemDTO/ItemRequestDTO.java
@@ -1,19 +1,9 @@
 package br.com.loja_gp2.loja_gp2.dto.ItemDTO;
 
-import br.com.loja_gp2.loja_gp2.dto.PedidoDTO.PedidoRequestDTO;
 import br.com.loja_gp2.loja_gp2.dto.ProdutoDTO.ProdutoRequestDTO;
 
 public class ItemRequestDTO extends ItemBaseDTO {
-    private PedidoRequestDTO pedido;
     private ProdutoRequestDTO produto;
-    
-    public PedidoRequestDTO getPedido() {
-        return pedido;
-    }
-
-    public void setPedido(PedidoRequestDTO pedido) {
-        this.pedido = pedido;
-    }
 
     public ProdutoRequestDTO getProduto() {
         return produto;
--- loja_gp2/src/main/java/br/com/loja_gp2/loja_gp2/service/ItemService.java
+++ loja_gp2/src/main/java/br/com/loja_gp2/loja_gp2/service/ItemService.java
@@ -1,5 +1,70 @@
 package br.com.loja_gp2.loja_gp2.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.loja_gp2.loja_gp2.dto.ItemDTO.ItemRequestDTO;
+import br.com.loja_gp2.loja_gp2.dto.ItemDTO.ItemResponseDTO;
+import br.com.loja_gp2.loja_gp2.dto.ProdutoDTO.ProdutoResponseDTO;
+import br.com.loja_gp2.loja_gp2.model.exceptions.ResourceBadRequestException;
+import br.com.loja_gp2.loja_gp2.model.modelPuro.Item;
+import br.com.loja_gp2.loja_gp2.model.modelPuro.Pedido;
+import br.com.loja_gp2.loja_gp2.model.modelPuro.Produto;
+import br.com.loja_gp2.loja_gp2.repository.ItemRepository;
+
+@Service
 public class ItemService {
     
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @Autowired
+    private ProdutoService produtoService;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+
+    public List<ItemResponseDTO> cadastrarItensPedido (Pedido pedido, List<ItemRequestDTO> listaItensReq) {
+        List<Item> listaItens = new ArrayList<>();
+        
+        for (ItemRequestDTO itemReq : listaItensReq) {
+            
+            // verifica se não é possivel realizar a venda para aquele produto
+            /*
+             * 
+             if (itemReq.getQuantidade() > produtoService.verificarEstoque(itemReq.getProduto()) {
+                 throw new ResourceBadRequestException("Item", "Estoque indisponivel para o produto com Id: "+itemReq.getProduto().getId());
+                }
+            */
+
+            Produto produtoDoItem = modelMapper.map(itemReq.getProduto(), Produto.class);
+            Item item = modelMapper.map(itemReq, Item.class);
+            
+
+            item.setProduto(produtoDoItem);
+
+            // retirar do estoque de produto. o estoque de produto dentro de item vai estar desatualizado, discutir se deve ou não atualizar com os outros
+            //produtoService.retirarEstoque(item.getQuantidade());
+
+
+
+
+
+           // listaItensResponse.add()
+
+
+           
+        }
+        
+        return null;
+
+
+
+    }
 }
