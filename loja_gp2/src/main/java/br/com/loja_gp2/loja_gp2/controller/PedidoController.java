5437a8d0adca68c37bcdddaa50a3a68dcb952f19
Dayane Thuller <dada_thuller@yahoo.com.br>
2023-10-17 16:32:57 -0300

    implementação dos métodos básicos de PedidoController

--- loja_gp2/src/main/java/br/com/loja_gp2/loja_gp2/controller/PedidoController.java
+++ loja_gp2/src/main/java/br/com/loja_gp2/loja_gp2/controller/PedidoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.loja_gp2.loja_gp2.dto.PedidoDTO.PedidoRequestDTO;
 import br.com.loja_gp2.loja_gp2.dto.PedidoDTO.PedidoResponseDTO;
-import br.com.loja_gp2.loja_gp2.dto.ProdutoDTO.ProdutoResponseDTO;
 import br.com.loja_gp2.loja_gp2.service.PedidoService;
 
 @RestController
@@ -23,11 +23,26 @@ public class PedidoController {
     @Autowired
     private PedidoService pedidoService;
 
+    @GetMapping
+    public ResponseEntity<List<PedidoResponseDTO>> getAll() {
+
+        List<PedidoResponseDTO> listaPedido = pedidoService.buscarTodosPedidos();
+
+        return ResponseEntity.ok(listaPedido);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PedidoResponseDTO> getById(@PathVariable Long id) {
+
+        PedidoResponseDTO pedidoEncontrado = pedidoService.buscarPedidoPorId(id);
+
+        return ResponseEntity.status(HttpStatus.FOUND).body(pedidoEncontrado);
+    }
 
     @PostMapping
-    public ResponseEntity<PedidoResponseDTO> postOne (@RequestBody PedidoRequestDTO pedidoRequest) {
-        PedidoResponseDTO pedidoResponse = pedidoService.cadastrarPedido(pedidoRequest);
+    public ResponseEntity<PedidoResponseDTO> postOne (@RequestBody PedidoRequestDTO pedido) {
+        PedidoResponseDTO pedidoCriado = pedidoService.cadastrarPedido(pedido);
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(pedidoResponse);
+        return ResponseEntity.status(HttpStatus.CREATED).body(pedidoCriado);
     }   
 }
